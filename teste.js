927ec2525680ffc2c96b17d170c3b1c5a7b30eba
Paulo Henrique Estevez <91810319+RickEstevez@users.noreply.github.com>
2024-04-05 09:50:41 -0300

    Adição da pasta de exercícios da Aula 3

---
+++ teste.js
@@ -0,0 +1,21 @@
+let a = 1
+let b = 8.9
+
+let variavel = '11.3'
+console.log(typeof variavel)
+
+let numero = +variavel
+console.log(typeof numero)
+
+let c = a+b
+let d = a-b
+let e = a*b
+let f = a/b
+
+resto = a%b
+
+console.log(c)
+// console.log(d)
+console.log(e)
+console.log(f)
+console.log(resto)
---
+++ teste.py
@@ -0,0 +1,4 @@
+# userInput = 
+entradaUsuario = input("Informe seu salário: ")
+
+print('O salário informado é de: ' + entradaUsuario)
